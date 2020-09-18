e1324b947648b032a4fb9f20f645caa2cbd94c7c
Leo Wang <leo.wang@massive.co>
2020-09-18 11:15:10 +1000

    fix 3 digit color render

--- samples-storyboard/samples-storyboard/Styles.swift
+++ samples-storyboard/samples-storyboard/Styles.swift
@@ -17,19 +17,22 @@ enum Styles {
 
 extension UIColor {
 
-    // https://www.iosapptemplates.com/blog/swift-programming/convert-hex-colors-to-uicolor-swift-4
+    // https://stackoverflow.com/a/56874327/11727285
     convenience init(hex: String, alpha: CGFloat = 1.0) {
-        let hexString: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        let scanner = Scanner(string: hexString)
-        if (hexString.hasPrefix("#")) {
-            scanner.scanLocation = 1
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (r, g, b) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (1, 1, 1)
         }
-        var color: UInt32 = 0
-        scanner.scanHexInt32(&color)
-        let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
         let red   = CGFloat(r) / 255.0
         let green = CGFloat(g) / 255.0
         let blue  = CGFloat(b) / 255.0
